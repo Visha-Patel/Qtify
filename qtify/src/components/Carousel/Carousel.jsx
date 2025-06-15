a4c602dee6ed3cc0f94122f853df603227157041
vishal <vp151397@gmail.com>
2025-06-15 21:55:59 +0530

    update

--- qtify/src/components/Carousel/Carousel.jsx
+++ qtify/src/components/Carousel/Carousel.jsx
@@ -1,10 +1,29 @@
 import React, { useState, useEffect } from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
+import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
 import { Navigation } from "swiper/modules";
 import styles from "./Carousel.module.css";
 import "swiper/css";
 import CarouselLeftNavigation from "./CarouselLeftNavigation/CarouselLeftNavigation";
 import CarouselRightNavigation from "./CarouselRightNavigation/CarouselRightNavigation";
+
+function CarouselControls({ handleRightClick }) {
+  const swiper = useSwiper();
+
+  useEffect(() => {
+    if (swiper) {
+      swiper.slideTo(0);
+    }
+  }, [swiper]);
+
+  return (
+    <div>
+      <CarouselLeftNavigation />
+      <div onClick={handleRightClick}>
+        <CarouselRightNavigation />
+      </div>
+    </div>
+  );
+}
 
 function Carousel({ data: initialData, renderComponent }) {
   const [data, setData] = useState(initialData);
@@ -13,8 +32,8 @@ function Carousel({ data: initialData, renderComponent }) {
   const handleRightClick = () => {
     setClickCount((prevCount) => prevCount + 1);
     if (clickCount === 3) {
-      setData((prevData) => prevData.slice(2));
-    //   setClickCount(0);
+      setData((prevData) => prevData.slice(1));
+      setClickCount(0);
     }
   };
 
@@ -28,12 +47,7 @@ function Carousel({ data: initialData, renderComponent }) {
         spaceBetween={40}
         allowTouchMove
       >
-        <div>
-          <CarouselLeftNavigation />
-          <div onClick={handleRightClick}>
-            <CarouselRightNavigation />
-          </div>
-        </div>
+        <CarouselControls handleRightClick={handleRightClick} />
         {data.map((ele) => (
           <SwiperSlide key={ele.id}>{renderComponent(ele)}</SwiperSlide>
         ))}
