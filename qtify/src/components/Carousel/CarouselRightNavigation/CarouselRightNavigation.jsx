8d6bdc88df876e67c00574777b03aac9742ca8c2
vishal <vp151397@gmail.com>
2025-06-16 23:41:09 +0530

    changes

--- qtify/src/components/Carousel/CarouselRightNavigation/CarouselRightNavigation.jsx
+++ qtify/src/components/Carousel/CarouselRightNavigation/CarouselRightNavigation.jsx
@@ -2,27 +2,29 @@ import React, { useState, useEffect } from "react";
 import { useSwiper } from "swiper/react";
 import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
 import styles from "./CarouselRightNavigation.module.css";
-
-function CarouselRightNavigation({ handleRightClick }) {
+ 
+function CarouselRightNavigation() {
   const swiper = useSwiper();
   const [isEnd, setIsEnd] = useState(false);
-
+ 
   useEffect(() => {
     const handleChange = () => {
       setIsEnd(swiper.isEnd);
     };
+ 
     setIsEnd(swiper.isEnd);
     swiper.on("slideChange", handleChange);
+ 
     return () => {
       swiper.off("slideChange", handleChange);
     };
   }, [swiper]);
-
+ 
   return (
     <div className={styles.rightNavigation}>
       <KeyboardArrowRightIcon
         className={styles.arrow}
-        onClick={handleRightClick}
+        onClick={() => !isEnd && swiper.slideNext()}
         aria-label="Next Slide"
         data-cy="carousel-next-button"
         style={{
@@ -33,5 +35,5 @@ function CarouselRightNavigation({ handleRightClick }) {
     </div>
   );
 }
-
+ 
 export default CarouselRightNavigation;
